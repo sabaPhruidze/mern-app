6a515f408fedb3aaa22292b5b249ee256ec62756
Saba Phruidze <sabaphruidze01@gmail.com>
2026-01-20 18:49:58 +0400

    added react-router-dom and axios which will definetly be needed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="w-50 h-50 bg-green-500">
       <p>Hi how are you</p>
