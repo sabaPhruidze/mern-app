83820c8442560fae4bfdc3fe6f6520983ca3bb2d
Saba <sabaphruidze01@gmail.com>
2026-01-22 18:47:32 +0400

    now I added axios and post works, currently I will do it locally and than I will add backend on render so I can display the project

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -3,17 +3,43 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import type { RegisterSchema } from "../schemas/register";
 import { registerSchema } from "../schemas/register";
 import { inputData } from "../constants/registerInputs";
+import { useNavigate } from "react-router-dom";
+import axios from 'axios'
 
 const Register = () => {
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    setError, // for Server error
+    formState: { errors, isSubmitting }, //for is submitting button
     reset,
   } = useForm<RegisterSchema>({ resolver: zodResolver(registerSchema) });
-  const onSubmit = (data: RegisterSchema) => {
-    console.log(data);
+  const onSubmit = async (data: RegisterSchema) => {
+    try {
+      const response = await axios.post('http://localhost:3000/api/users/register',{
+        name:data.name,
+        email:data.email,
+        password:data.password,
+        confirmPassword:data.confirmPassword
+      })
+      if(response.data) {
+        localStorage.setItem('user',JSON.stringify(response.data));
+        reset();
+        navigate('/')
+      }
     reset();
+    } catch (error) {
+     if(axios.isAxiosError(error)) {
+      const message =error.response?.data?.message;
+      if (message === "User already exists" || message?.includes("exists")) {
+          setError("email", { type: "server", message: message });
+        } else {
+           // სხვა ზოგადი ერორი (შეგიძლია alert გამოიყენო ან ცალკე ველში გამოიტანო)
+           alert(message || "Something went wrong");
+        }
+     }
+    }
   };
   
   const inputclasses =
@@ -36,7 +62,7 @@ const Register = () => {
                 {...register(item.register)}
                 className={inputclasses}
               />
-              {errors[item.register]?.message === undefined ? "" : (
+              {errors[item.register] && (
                 <p className="text-red-500 text-sm">
                 {String(errors[item.register]?.message)}
               </p>
@@ -44,8 +70,8 @@ const Register = () => {
               
             </div>
           ))}
-          <button className="bg-black text-white py-3 rounded font-bold hover:bg-gray-800 mt-2 cursor-pointer">
-            Register
+          <button className="bg-black text-white py-3 rounded font-bold hover:bg-gray-800 mt-2 cursor-pointer" disabled={isSubmitting} type="submit">
+            {isSubmitting ? "Loading..." : 'Register'}
           
           </button>
         </form>
