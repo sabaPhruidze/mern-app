d5f2e74e7370f249b21e5988e4789a6efd6cde0f
Saba <sabaphruidze01@gmail.com>
2026-01-24 09:05:48 +0400

    now I imported it in my header and when someone login or register their name will appeal on top left and logout button as well, and after clicking it it will be removed

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
+import { useAppSelector,useAppDispatch } from "../store/store";
 import { FaSignInAlt, FaUser } from "react-icons/fa";
+import { logout,reset } from "../store/slices/authSlice";
 const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const {user} = useAppSelector((state) => state.auth);
+  
+  const onLogout = () => {
+    dispatch(logout());//remove user and token from localStorage
+    dispatch(reset()); // cleaning status
+    navigate('/login')//navigate on login page
+  }
   return (
     <header className="bg-gray-800 text-white p-4 mb-10 header">
       <div className="container mx-auto flex justify-between items-center">
@@ -10,7 +21,21 @@ const Header = () => {
         </div>
         {/* menu botton */}
         <ul className="flex items-center space-x-6">
-          <li className="flex items-center gap-2 hover:text-gray-300">
+         {user ? (
+           <>
+           <li className="font-medium text-gray-300">
+            Hi, {user.name}
+           </li>
+           <li>
+            <button onClick={onLogout} className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition cursor-pointer">
+              Logout
+            </button>
+           </li>
+           </>
+         )
+         : (
+           <>
+           <li className="flex items-center gap-2 hover:text-gray-300">
             <Link to="/login">
               <FaSignInAlt /> Login
             </Link>
@@ -20,6 +45,8 @@ const Header = () => {
               <FaUser /> Register
             </Link>
           </li>
+           </>
+         )}
         </ul>
       </div>
     </header>
