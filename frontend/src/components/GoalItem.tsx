86d60b0778e2cfe42046b9bb9fbd0e8e49d3ec7d
Saba <sabaphruidze01@gmail.com>
2026-01-25 14:40:25 +0400

    front is build but as I said for my mistake I forgot backend and I will built it next time

--- frontend/src/components/GoalItem.tsx
+++ frontend/src/components/GoalItem.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { useAppDispatch } from "../store/store"
+import { deleteGoals, } from "../store/slices/goalSlices"
+import type { Goal } from "../store/slices/goalSlices"
 
-const GoalItem = () => {
+interface GoalItemProps {
+    goal:Goal;
+}
+ const GoalItem = ({goal}:GoalItemProps) => {
+    const dispatch = useAppDispatch();
+    const date = new Date(goal.createdAt).toLocaleDateString('en-US');
+    // for georgian ka-GE
   return (
-    <div>GoalItem</div>
+    <div className="bg-white border border-gray-200 p-4 rounded-lg shadow-sm flex justify-between items-center relative group hover:shadow-md transition duration-300">
+        <div>
+            <p className="text-xs text-gray-400 mb-1 font-mono">
+            {date}
+            </p>
+        <h2 className="text-lg font-bold text-gray-800 break-all">{goal.text}</h2>
+        <button onClick={() => dispatch(deleteGoals(goal._id))}
+            className="text-gray-400 hover:text-red-600 font-bold p-2 rounded transition cursor-pointer"
+            title="Remove"
+            >
+            X
+        </button>
+        </div>
+    </div>
   )
 }
 
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,11 +1,50 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAppSelector,useAppDispatch } from "../store/store";
 import GoalForm from "../components/GoalForm";
-
+import GoalItem from "../components/GoalItem";
+import { getGoals,reset } from "../store/slices/goalSlices";
 const Dashboard = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const {user} = useAppSelector(state => state.auth);
+  const {goals,isLoading,isError,message} = useAppSelector(state => state.goals);
+  
+  useEffect((() => {
+    if(isError) {
+      console.log(message);
+    }
+    if(!user) {
+      navigate('/login');
+    }else {
+      dispatch(getGoals());
+    }
+
+    return () => {
+      dispatch(reset());
+    }
+  }),[user,navigate,isError,message,dispatch])
+
+  if(isLoading) {
+    return <div className="text-center mt-20 text-2xl animate-pulse">Loading ...</div>
+  }
+  
   return (
-    <div className="text-3xl font-bold text-center mt-10">
-      <div>
-        <GoalForm/>
-      </div>
+    <div className="w-full max-w-4xl mx-auto p5">
+      <section className="mb-10 text-center">
+        <h1 className="text-4xl font-extrabold text-gray-800 mb-2">Hi {user && user.name}</h1>
+        <p className="text-gray-500 text-lg">Goals Dashboard</p>
+      </section>
+      <GoalForm/>
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {goals.length > 0 ? (
+          goals.map((goal) => <GoalItem key={goal._id } goal={goal}/>)
+        ): (
+          <div className="col-span-2 text-center py-10 bg-gray-50 rounded-lg border border-dashed border-gray-300">
+            <h3 className="text-gray-400 text-lg">No goals added yet</h3>
+          </div>
+        )}
+      </section>
     </div>
   );
 };
