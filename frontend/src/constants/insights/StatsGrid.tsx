5861f0b7e533cbed41cdb983e7dd5d0727342fdd
Saba <sabaphruidze01@gmail.com>
2026-01-26 20:06:34 +0400

    in order not to repeat data I made a massive and used map for cards

--- frontend/src/constants/insights/StatsGrid.tsx
+++ frontend/src/constants/insights/StatsGrid.tsx
@@ -1,13 +1,29 @@
 import StatsCard from "./statsCard"
 
+interface CardItem {
+    title:string;
+    value:string | number;
+    hint:string;
+}
+
 const StatsGrid = () => {
+     const cards: CardItem[] = [
+    { title: "Total Goals", value: 12, hint: "All time" },
+    { title: "Today", value: 2, hint: "Created today" },
+    { title: "Last 7 Days", value: 6, hint: "Weekly activity" },
+    { title: "Top Day", value: "2026-01-26", hint: "Most active" },
+  ];
   return (
     <div className='max-w-3xl mx-auto px-4 mt-6'>
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4'>
-        <StatsCard title="Total Goals" value={12} hint="All time"/>
-        <StatsCard title="Today" value={2} hint="Created today" />
-        <StatsCard title="Last 7 Days" value={6} hint="Weekly activity" />
-        <StatsCard title="Top Day" value="2026-01-26" hint="Most active" />
+        {cards.map((item) => (
+            <StatsCard 
+            key={item.title} 
+            title={item.title} 
+            value={item.value} 
+            hint={item.hint} />
+        ))}
+        
         </div>
     </div>
   )
