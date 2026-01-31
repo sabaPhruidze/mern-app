4b5f7461c27e933f7eb139e9069995fb8096d9a3
Saba <sabaphruidze01@gmail.com>
2026-01-31 22:34:46 +0400

    I want to test with RTL insights but there are many things connected to redux and it is not the level I am currently so I will move on components and Filterbar in insightsView and than test this component

---
+++ frontend/src/components/insights/InsightsView.tsx
@@ -0,0 +1,13 @@
+import { useMemo, useRef, useState } from "react";
+import StatsGrid from "./StatsGrid";
+import TopDaysList from "./TopDaysList";
+import InsightsFilterBar from "./InsightsFilterBar";
+import type { CardItem } from "./StatsGrid";
+
+export type RangeType = "all" | "7d";
+
+const InsightsView = () => {
+  return <div>InsightsView</div>;
+};
+
+export default InsightsView;
