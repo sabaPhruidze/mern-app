5f4dfc8063be254fbaa1724603feb7e74ce3027e
Saba <sabaphruidze01@gmail.com>
2026-02-02 18:27:36 +0400

    now I will one more time search what needs to be cleaned

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,14 +1,22 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import tailwindcss from "eslint-plugin-tailwindcss";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      tailwindcss,
+    },
+    rules: {
+      "tailwindcss/classnames-order": "warn",
+      "tailwindcss/no-custom-classname": "warn",
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -20,4 +28,4 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+]);
