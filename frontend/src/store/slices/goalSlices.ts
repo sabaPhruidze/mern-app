b8572a641275006ca699b3ae21805574d9dd118b
Saba <sabaphruidze01@gmail.com>
2026-02-03 13:05:18 +0400

    I think it is better way of this ,since if error came and than I succeed the error message might still be there ,but this way it will not be definetly

--- frontend/src/store/slices/goalSlices.ts
+++ frontend/src/store/slices/goalSlices.ts
@@ -1,141 +1,158 @@
-import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { AxiosError } from "axios";
-import  goalServices from "../../features/goals/goalService";
+import goalServices from "../../features/goals/goalService";
 import type { Goalpayload } from "../../features/goals/goalService";
 import type { BackendError } from "./authSlice";
 export interface Goal {
-    _id:string;
-    text:string;
-    createdAt: string;
+  _id: string;
+  text: string;
+  createdAt: string;
 }
 interface GoalState {
-    goals:Goal[];
-    isError:boolean;
-    isSuccess:boolean;
-    isLoading:boolean;
-    message:string;
+  goals: Goal[];
+  isError: boolean;
+  isSuccess: boolean;
+  isLoading: boolean;
+  message: string;
 }
-const initialState:GoalState = {
-    goals:[],
-    isError:false,
-    isSuccess:false,
-    isLoading:false,
-    message:''
-}
-const getErrorMessage = (error:unknown) => {
-    const err = error as AxiosError<BackendError>;
-    return err.response?.data?.message || err.message || 'Something went wrong'
-}
-export const createGoal = createAsyncThunk<Goal,Goalpayload,{rejectValue:string}>(
-    'goals/create',async(goalData,thunkAPI) => {
-        try {
-            return await goalServices.createGoal(goalData);
-        } catch (error) {
-            return thunkAPI.rejectWithValue(getErrorMessage(error))
-        }
-    }
-)
-export const getGoals = createAsyncThunk<Goal[],void,{rejectValue:string}>(
-    'goals/getAll',async(_,thunkAPI) => {
-        try {
-            return await goalServices.getGoals();
-        } catch (error) {
-            return thunkAPI.rejectWithValue(getErrorMessage(error))
-        }
-    }
-)
-export const updateGoal = createAsyncThunk<Goal,{id:string,data:Goalpayload},{rejectValue:string}>('goals/update',async({id,data},thunkAPI) => {
+const initialState: GoalState = {
+  goals: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+const getErrorMessage = (error: unknown) => {
+  const err = error as AxiosError<BackendError>;
+  return err.response?.data?.message || err.message || "Something went wrong";
+};
+export const createGoal = createAsyncThunk<
+  Goal,
+  Goalpayload,
+  { rejectValue: string }
+>("goals/create", async (goalData, thunkAPI) => {
+  try {
+    return await goalServices.createGoal(goalData);
+  } catch (error) {
+    return thunkAPI.rejectWithValue(getErrorMessage(error));
+  }
+});
+export const getGoals = createAsyncThunk<Goal[], void, { rejectValue: string }>(
+  "goals/getAll",
+  async (_, thunkAPI) => {
     try {
-        return await goalServices.updateGoals(id,data);
+      return await goalServices.getGoals();
     } catch (error) {
-        return thunkAPI.rejectWithValue(getErrorMessage(error))
+      return thunkAPI.rejectWithValue(getErrorMessage(error));
     }
-})
-export const deleteGoals = createAsyncThunk<string,string,{rejectValue:string}>( 
-    "goals/delete", async (goalId, thunkAPI) => {
-        try {
-            await goalServices.deleteGoal(goalId);
-            return goalId;
-        } catch (error) {
-            return thunkAPI.rejectWithValue(getErrorMessage(error))
-        }
-    }
-)
+  },
+);
+export const updateGoal = createAsyncThunk<
+  Goal,
+  { id: string; data: Goalpayload },
+  { rejectValue: string }
+>("goals/update", async ({ id, data }, thunkAPI) => {
+  try {
+    return await goalServices.updateGoals(id, data);
+  } catch (error) {
+    return thunkAPI.rejectWithValue(getErrorMessage(error));
+  }
+});
+export const deleteGoals = createAsyncThunk<
+  string,
+  string,
+  { rejectValue: string }
+>("goals/delete", async (goalId, thunkAPI) => {
+  try {
+    await goalServices.deleteGoal(goalId);
+    return goalId;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(getErrorMessage(error));
+  }
+});
 
 export const goalSlice = createSlice({
-    name:'goals',
-    initialState,
-    reducers: {
-        // ordinary sync reducers
-        reset:(state) => {
-            state.isError = false;
-            state.isSuccess=false;
-            state.isLoading=false;
-            state.message='';
-        },
+  name: "goals",
+  initialState,
+  reducers: {
+    // ordinary sync reducers
+    reset: (state) => {
+      state.isError = false;
+      state.isSuccess = false;
+      state.isLoading = false;
+      state.message = "";
     },
-    // updating on thunks lifecycle
-    extraReducers:(builder) => {
-        builder
-        .addCase(createGoal.pending,(state) => {
-            state.isLoading = true;
-        })
-        .addCase(createGoal.fulfilled,(state,action) => {
-            state.isLoading= false;
-            state.isSuccess = true;
-            state.goals.push(action.payload)
-        })
-        .addCase(createGoal.rejected,(state,action) => {
-            state.isLoading = false;
-            state.isSuccess= false;
-            state.isError = true;
-            state.message = action.payload as string;
-        }) 
-        .addCase(getGoals.pending,(state) => {
-            state.isLoading =  true;
-        })
-        .addCase(getGoals.fulfilled,(state,action) => {
-            state.isLoading=false;
-            state.isSuccess =true;
-            state.goals = action.payload;
-        })
-        .addCase(getGoals.rejected,(state,action) => {
-            state.isLoading = false;
-            state.isSuccess = false;
-            state.isError = true;
-            state.message = action.payload as string;
-        })
-        .addCase(updateGoal.pending,(state) => {
-            state.isLoading = true;
-        })
-        .addCase(updateGoal.fulfilled,(state,action) => {
-            state.isLoading = false;
-            state.isSuccess = true;
-            state.goals = state.goals.map((g) =>  g._id === action.payload._id ? action.payload : g)
-        })
-        .addCase(updateGoal.rejected,(state,action) => {
-            state.isLoading = false;
-            state.isSuccess = false;
-            state.isError = true;
-            state.message = action.payload as string;
-        })
-        .addCase(deleteGoals.pending,(state) => {
-            state.isLoading = true;
-        })
-        .addCase(deleteGoals.fulfilled,(state,action) => {
-            state.isLoading=false;
-            state.isSuccess=true;
-            state.goals = state.goals.filter(goal => goal._id !== action.payload);            
-        })
-        .addCase(deleteGoals.rejected,(state,action) => {
-            state.isLoading = false;
-            state.isSuccess=false;
-            state.isError =true;
-            state.message = action.payload as string;
-        })
-    }
-    
-    
-})
-export const {reset} = goalSlice.actions;
+  },
+  // updating on thunks lifecycle
+  extraReducers: (builder) => {
+    builder
+      .addCase(createGoal.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createGoal.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.isError = false;
+        state.message = "";
+        state.goals.push(action.payload);
+      })
+      .addCase(createGoal.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = false;
+        state.isError = true;
+        state.message = action.payload as string;
+      })
+      .addCase(getGoals.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getGoals.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.isError = false;
+        state.message = "";
+        state.goals = action.payload;
+      })
+      .addCase(getGoals.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = false;
+        state.isError = true;
+        state.message = action.payload as string;
+      })
+      .addCase(updateGoal.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(updateGoal.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.isError = false;
+        state.message = "";
+        state.goals = state.goals.map((g) =>
+          g._id === action.payload._id ? action.payload : g,
+        );
+      })
+      .addCase(updateGoal.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = false;
+        state.isError = true;
+        state.message = action.payload as string;
+      })
+      .addCase(deleteGoals.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteGoals.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.isError = false;
+        state.message = "";
+        state.goals = state.goals.filter((goal) => goal._id !== action.payload);
+      })
+      .addCase(deleteGoals.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = false;
+        state.isError = true;
+        state.message = action.payload as string;
+      });
+  },
+});
+export const { reset } = goalSlice.actions;
 export default goalSlice.reducer;
