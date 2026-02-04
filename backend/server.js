be089022dd172f2c22494766c298d7875893090d
Saba <sabaphruidze01@gmail.com>
2026-02-04 22:47:37 +0400

    update

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ connectDB();
 // Middleware
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: [process.env.CORS_ORIGIN, "http://localhost:5173"],
     credentials: true,
   }),
 );
